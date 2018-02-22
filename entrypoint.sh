a0fe7daefdcd139319f2f542a0c04ba8c30f3061
Jakob Müller <j.mueller@oulook.de>
2018-02-22 17:07:24 +0100

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,14 @@
 usermod -a -G sudo c9
 echo c9:"$c9_password" | /usr/sbin/chpasswd
 
-su c9
-
 cd ~
 mkdir ".ssh"
 mkdir "workspace"
 
 cd ".ssh"
 echo "$c9_sshkey" > authorized_keys
+
+cd ~
+chown -R c9:c9 *
 
 service ssh start
